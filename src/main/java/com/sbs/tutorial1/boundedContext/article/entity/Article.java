30a155d939644d84993639f65d64e5275b50c573
SangWon7242 <gotkdqja@gmail.com>
2024-12-08 11:32:18 +0900

    Article 클래스에 createDate, modifyDate 변수 추가

--- src/main/java/com/sbs/tutorial1/boundedContext/article/entity/Article.java
+++ src/main/java/com/sbs/tutorial1/boundedContext/article/entity/Article.java
@@ -1,12 +1,22 @@
 package com.sbs.tutorial1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Data
 public class Article {
   @Id // primary key가 id 칼럼 적용
+  @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
   private long id; 
+  private LocalDateTime createDate; // 데이터 생성 날짜
+  private LocalDateTime modifyDate; // 데이터 수정 날짜
   private String subject;
   private String content;
 }
