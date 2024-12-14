40ee51e8786d5da9bb5c592d88027439ccf9325c
SangWon7242 <gotkdqja@gmail.com>
2024-12-14 10:14:03 +0900

    member 엔티티 도입, 회원 데이터를 영속화

--- src/main/java/com/sbs/tutorial1/boundedContext/member/repository/MemberRepository.java
+++ src/main/java/com/sbs/tutorial1/boundedContext/member/repository/MemberRepository.java
@@ -1,42 +1,10 @@
 package com.sbs.tutorial1.boundedContext.member.repository;
 
 import com.sbs.tutorial1.boundedContext.member.entity.Member;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
-@Repository // 컴포넌트랑 같다.
-public class MemberRepository {
-  private List<Member> members;
-
-  public MemberRepository() {
-    members = new ArrayList<>();
-
-    members.add(new Member("user1", "1234"));
-    members.add(new Member("abc", "123456"));
-    members.add(new Member("test", "1222"));
-    members.add(new Member("bbc", "1111"));
-    members.add(new Member("love", "5555"));
-    members.add(new Member("hello", "123478"));
-    members.add(new Member("user2", "12596"));
-    members.add(new Member("user3", "33333"));
-    members.add(new Member("good", "456732"));
-    members.add(new Member("like", "123444"));
-  }
-
-
-  public Member findByUserName(String username) {
-    return members.stream()
-        .filter(member -> member.getUsername().equals(username))
-        .findFirst()
-        .orElse(null);
-  }
-
-  public Member findById(long id) {
-    return members.stream()
-        .filter(member -> member.getId() == id)
-        .findFirst()
-        .orElse(null);
-  }
+public interface MemberRepository extends JpaRepository<Member, Long> {
+  Optional<Member> findByUsername(String username);
 }
